3de864cd37aeb990c3d6b5bf954db613c187489e
gompro <junhyung3224@gmail.com>
2019-10-25 23:28:42 +0900

    add

---
+++ pre-commit.js
@@ -0,0 +1,33 @@
+const { exec } = require('child_process')
+
+exec('git diff --cached --name-only --diff-filter=ACM', (err, stdout, stderr) => {
+    if (err) {
+      console.error('error:', err);
+      return;
+    }
+
+    const matchFilesUnderServer = /server\/(.+\.js)/g
+
+    if (matchFilesUnderServer.test(stdout)) {
+        console.log('hey this guy changed so will copy this!')
+        const matched = stdout.match(matchFilesUnderServer)
+
+        for (const source of matched) {
+            const dest = file.replace('server/', 'client/')
+            exec(`cp ${source} ${dest}`, (err, stdout, stderr) => {
+                if (err) {
+                    console.log('failed to copy!', err)
+                    return;
+                }
+                exec(`git add ${dest}`, (err, stdout, stderr) => {
+                    if (err) {
+                        console.log('failed to add file', err)
+                        return;
+                    }
+                })
+            })
+        }
+    }
+
+    console.log(stdout);
+});
